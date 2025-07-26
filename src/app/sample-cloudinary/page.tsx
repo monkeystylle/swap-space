fb60bfa3b9aa469ab2e184f90327ffbb2294b6cd
meng <monkeystylle@gmail.com>
2025-07-27 01:57:24 +0800

    Revert " fix badge on conversation list"
    This reverts commit 7619c5cd479ae8159b79047575cf559c5c851156.

---
+++ src/app/sample-cloudinary/page.tsx
@@ -0,0 +1,11 @@
+import SampleCloudinary from '@/features/bartering/components/sample-cloudinary';
+
+const SampleCloudinaryPage = () => {
+  return (
+    <div className="flex-1 flex flex-col justify-center items-center">
+      <SampleCloudinary />
+    </div>
+  );
+};
+
+export default SampleCloudinaryPage;
